202381c29d7cfa9d63e6773a1ed36929fbb58ed8
Charkapong <charkapong@hotmail.com>
2017-03-17 20:44:09 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'lwm3y2+EzxWTXxa2KEK/zj2lwqxHqtGJvnIlwgaaOhlwW1MqXvw+ph1+j4G49QszXD+BS2Q+l6V8cl4ZW2jTDcA26AiYVw/BnGCI9WKg2MyL7znYWVC3tVpqZJ1rbsl2T75V0Mour7UJy+KAkpxtcQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
